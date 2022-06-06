f3176add610b1d63407c5815b8d9780d5d13bf53
arian-esl <itsarianes@gmail.com>
2022-06-06 21:35:14 +0430

    Added boring code

---
+++ iteration_names_example.py
@@ -0,0 +1,11 @@
+# Boring example :|
+names = ["user0", "user1", "user2", "user3"]
+
+count = 0
+for name in names:
+    print('Hello', name)
+    count += 1
+
+print('I said', count, 'hellos')
+
+input("Press Enter to exit :|")
