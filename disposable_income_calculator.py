6b45a93d9940ea50af5d28aa6f12529e89d8a87d
arian-esl <itsarianes@gmail.com>
2022-06-06 21:22:28 +0430

    Added comments

--- disposable_income_calculator.py
+++ disposable_income_calculator.py
@@ -1,24 +1,20 @@
-# This programme will get Hour and Rate per hour from user 
-# and calculate Monthly income and salary after 10 percent tax 
-
-def income(hour, per_hour): # Calculator function
-    if hour > 8: # Check for standard working hour
+# Function which check inputs and calculate income and salary after 10% tax
+def income(hour, per_hour): 
+    if hour > 8: 
         print('Error, Too much work!')
-    elif per_hour < 15: # Check for standard rate per hour 
+    elif per_hour < 15: 
         print('Error, Your income is very low!')
-    else: # Calculate income and salary if hour and rate were standard 
-        total_income = (hour * per_hour) * 20 # Calculate total income for 20 days (standard working days)
-        disposable_income = total_income - total_income * 0.1 # Calculate disposable income (10% tax)
-        salary = disposable_income * 12 # Calculate salary
-        print('Your monthly income is: %i after 10 percent tax' %disposable_income) # Print monthly income
-        print('Your salary is: %i Dollars after 10 percent tax' %salary) # Print salary
+    else: 
+        total_income = (hour * per_hour) * 20 
+        disposable_income = total_income - total_income * 0.1 
+        salary = disposable_income * 12 
+        print('Your monthly income is: %i after 10 percent tax' %disposable_income) 
+        print('Your salary is: %i Dollars after 10 percent tax' %salary) 
 
-# Get input from user 
+# Driver code
 h = int(input('How many hour you are working per day? '))
 p = int(input('How much you earn per hour? '))
 
-# Calculator function apply on inputs
 income(h , p)
 
-# This is for keep running python file in windows OS
 input("Press enter to exit ;)")
